417789613292a0be0242bc775f60fdae677ace87
Pavle Milanov <pavle_milanov@hotmail.com>
2024-03-13 20:03:12 +0100

    Added basic navigation and started working on home page.

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Navigation',
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css'
+})
+export class NavigationComponent {
+
+  sidebarToggle:boolean = false;
+
+  toggle(){
+    this.sidebarToggle = !this.sidebarToggle;
+  }
+
+}
